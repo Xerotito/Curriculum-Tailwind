eb8ab2eacaa041c7791b08ed17dea1fe3bb3fe33
Victor Diaz <vitorx06@hotmail.com>
2023-02-09 17:32:53 -0300

    Aside casi terminada
    Quedaría agregar los certificados de cursos

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
-//° Transicion de opacidad del boton certificados en artículo cursos 
+//° Transicion de opacidad del boton certificados en setcion cursos
 
-const cursos = document.getElementById('cursos') //Delegamos el evento solamente cuando la seccion cursos este desplegada ()
-cursos.addEventListener('mouseover', (e) => e.target.matches('.certificado_btn') && e.target.firstElementChild.classList.remove('hidden'))    //Volvemos a agregar hidden
-cursos.addEventListener('mouseout', (e) => e.target.matches('.certificado_btn') && e.target.firstElementChild.classList.add('hidden'))    //Volvemos a agregar hidden
+const cursos = document.getElementById("cursos") //Delegamos el evento solamente cuando la seccion cursos este desplegada ()
+cursos.addEventListener("mouseover", e => e.target.matches(".certificado_btn") && e.target.firstElementChild.classList.remove("hidden")) //Volvemos a agregar hidden
+cursos.addEventListener("mouseout", e => e.target.matches(".certificado_btn") && e.target.firstElementChild.classList.add("hidden")) //Volvemos a agregar hidden
 
-/* Control de animaciones */
-const redes = document.getElementById('redes')
-redes.addEventListener('mouseover', (e) => {
-  console.log('en section redes')
+//° Animaciones section contacto
+document.addEventListener("mouseover", e => {
+  if (e.target.matches(".contacto_aside")) {    //Al pasar el mouse por elemento de contacto agregamos la clase css de la animación
+    const img = e.target.children[ 0 ]          //El primero hijo del elemnto es el logo correspondiente
+    img.classList.add("rotate360")
+    img.addEventListener("animationend", () => img.classList.remove("rotate360")) //Al terminar la animacion le sacamos la clase que anima con esto evitamos que se deje de reproducir al sacar el mouse
+  }
 })
